ff6194c22e158b0aeb7622bd48e4ed83effc04c7
Sony MelliFriady <zoenasun@gmail.com>
2023-01-05 17:45:39 +0700

    [update] employee routes

--- services/app/routes/employee.js
+++ services/app/routes/employee.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 
-const { getEmployees } = require("../controllers/employee");
+const {
+  getEmployees,
+  getEmployee,
+  insertEmployee,
+  updateEmployee,
+  destroyEmployee,
+} = require("../controllers/employee");
 
 router.get("/", getEmployees);
+router.post("/", insertEmployee);
+router.get("/:id", getEmployee);
+router.put("/:id", updateEmployee);
+router.delete("/:id", destroyEmployee);
 
 module.exports = router;
