5f41d93c13118803d2b3277a11c80e1426acf515
Sony MelliFriady <zoenasun@gmail.com>
2023-01-06 17:38:55 +0700

    [update] project

--- services/app/middlewares/bcrypt.js
+++
@@ -1,8 +0,0 @@
-const bcryptjs = require("bcryptjs");
-
-const hashPassword = (password) => bcryptjs.hashSync(password);
-
-const comparePassword = (password, hassPass) =>
-  bcryptjs.compareSync(password, hassPass);
-
-module.exports = { hashPassword, comparePassword };
