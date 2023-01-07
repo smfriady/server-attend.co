c785b9f2134c012a05aeb0ead28c9ebdfcdfdc2a
HidayatNM <hidayatnurmusa1@gmail.com>
2023-01-07 20:55:45 +0700

    [update] errorhandling for multer

--- services/app/middlewares/multer.js
+++ services/app/middlewares/multer.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage: storage }).single("attachment");
+const multerUploads = multer({ storage: storage });
 
 module.exports = { multerUploads };
