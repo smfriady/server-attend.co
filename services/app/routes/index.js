04d34605b39a2ccca834e9eca1f11db5e0808a5e
HidayatNM <hidayatnurmusa1@gmail.com>
2023-01-07 16:06:08 +0700

    [update] fix endpoints

--- services/app/routes/index.js
+++ services/app/routes/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 const employeeRouter = require("./employee");
 
+const API_WEB = "/api/v1/web";
+const API_MOBILE = "/api/v1/mobile";
+
 router.get("/", (_req, res, _next) =>
   res.status(200).json({ message: "Server is connected" })
 );
-
-router.use("/employees", employeeRouter);
+router.use(`${API_WEB}/employees`, employeeRouter);
 
 module.exports = router;
