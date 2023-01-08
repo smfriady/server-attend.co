975741a5c93b487895fc179e210f7e93a00326c9
Sony MelliFriady <zoenasun@gmail.com>
2023-01-08 12:40:13 +0700

    [update] indentasi

--- services/app/routes/employee.js
+++ services/app/routes/employee.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { multerUploads } = require("../middlewares/multer");
 
-const { multerUploads } = require("../middlewares/multer");
 const {
   getEmployees,
   getEmployee,
