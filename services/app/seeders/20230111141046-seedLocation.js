eff0003e1ddbe905e89090affbf8938150c8ca34
Rahmat <rahmathikaru03@gmail.com>
2023-01-11 21:13:33 +0700

    add seeder location

---
+++ services/app/seeders/20230111141046-seedLocation.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    let dataLocation = require("../data/location.json").map((el) => {
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+      return el;
+    });
+    await queryInterface.bulkInsert("Locations", dataLocation);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Locations", null);
+  },
+};
